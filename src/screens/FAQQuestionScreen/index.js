49f097d173124282aa2609b7c9e668edfbe49558
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-15 02:31:03 -0300

    Rendering structured text

--- src/screens/FAQQuestionScreen/index.js
+++ src/screens/FAQQuestionScreen/index.js
@@ -3,6 +3,9 @@ import { Footer } from '../../components/commons/Footer';
 import { Menu } from '../../components/commons/Menu';
 import { Box, Text, theme } from '../../theme/components';
 import { cmsService } from '../../infra/cms/cmsService';
+import { renderNodeRule, StructuredText } from 'react-datocms';
+import { isHeading } from 'datocms-structured-text-utils';
+
 
 export async function getStaticPaths() {
   return {
@@ -59,8 +62,6 @@ export default function FAQQuestionScreen({ title, content }) {
       >
         <Box
           styleSheet={{
-            display: 'flex',
-            gap: theme.space.x4,
             flexDirection: 'column',
             width: '100%',
             maxWidth: theme.space.xcontainer_lg,
@@ -71,10 +72,24 @@ export default function FAQQuestionScreen({ title, content }) {
             {title}
           </Text>
 
-          {/* <Box dangerouslySetInnerHTML={{ __html: content }} /> */}
-          <pre>
+          <StructuredText
+            data={content}
+            customNodeRules={[
+              renderNodeRule(isHeading, ({ node, children, key }) => {
+                const tag = `h${node.level}`;
+                const variant = `heading${node.level}`;
+                return (
+                  <Text tag={tag} variant={variant} key={key}>
+                    {children}
+                  </Text>
+                )
+              })
+            ]}
+          />
+          {/* <pre>
             {JSON.stringify(content, null, 4)}
-          </pre>
+          </pre> */}
+          {/* <Box dangerouslySetInnerHTML={{ __html: content }} /> */}
         </Box>
       </Box>
 
