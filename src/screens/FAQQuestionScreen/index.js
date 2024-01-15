e342b408fc806635a59d2d197a37b515b3ff54ea
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-01-15 15:23:39 -0300

    Creating our data provider

--- src/screens/FAQQuestionScreen/index.js
+++ src/screens/FAQQuestionScreen/index.js
@@ -5,6 +5,7 @@ import { Box, Text, theme } from '../../theme/components';
 import { cmsService } from '../../infra/cms/cmsService';
 import { renderNodeRule, StructuredText } from 'react-datocms';
 import { isHeading } from 'datocms-structured-text-utils';
+import CMSProvider from '../../infra/cms/CMSProvider';
 
 
 export async function getStaticPaths() {
@@ -46,7 +47,7 @@ export async function getStaticProps({ params, preview }) {
 
 export default function FAQQuestionScreen({ cmsContent }) {
   return (
-    <>
+    <CMSProvider cmsContent={cmsContent}>
       <Head>
         <title>FAQ - Alura</title>
       </Head>
@@ -96,6 +97,6 @@ export default function FAQQuestionScreen({ cmsContent }) {
       </Box>
 
       <Footer description={cmsContent.globalContent.globalFooter.description} />
-    </>
+    </CMSProvider>
   )
 }
